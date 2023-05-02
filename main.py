f2c88e62c252ceb4bb88fef628d87191c36fe471
1liochka1 <ila00001120@gmail.com>
2023-05-03 01:05:42 +0300

    create

---
+++ main.py
@@ -0,0 +1,98 @@
+import asyncio
+import datetime
+from loguru import logger
+import services
+
+
+
+async def get_last_prices(exchanges_set):
+
+    """Получаем цены нашего коина с каждой биржи"""
+    tasks = [exchange[0].fetch_ticker('SUI/USDT') for exchange in exchanges_set]
+    results = await asyncio.gather(*tasks)
+    return results
+
+async def check_price(exchanges, exchanges_set, range_start, range_end,insta_sell,percent, max_time_in_range):
+    """
+    Выставялем ордера на продажу,
+    если цена превысила диапазон или цена находится в диапазоне max_time_in_range секунд
+    """
+
+    prices = await get_last_prices(exchanges_set)
+
+    to_sell = []
+
+    for count, price in enumerate(prices):
+        current_price = price['last']
+
+        exchange_id = exchanges_set[count][0].id
+
+        if current_price >= insta_sell:
+            logger.info(f'{exchange_id} --- {current_price} --- Сливаю по инста прайсу')
+
+            for exchange in exchanges[exchange_id]:
+                to_sell.append(limit_sell_order(exchange, 'SUI/USDT', current_price*percent))
+
+        elif range_start <= current_price <= range_end:
+            datetime_now = datetime.datetime.now()
+
+            if exchanges_set[count][1] == 0:
+                lst = list(exchanges_set[count])
+                lst[1] = datetime_now
+                exchanges_set[count] = tuple(lst)
+
+            duration_time = (datetime_now - exchanges_set[count][1]).seconds
+            logger.info(f'{exchange_id} --- {current_price} --- Цена в диапазоне {duration_time}с / {max_time_in_range}c')
+
+
+            if duration_time > max_time_in_range:
+                for exchange in exchanges[exchange_id]:
+                    to_sell.append(limit_sell_order(exchange, 'SUI/USDT', current_price*percent))
+        else:
+            logger.info(f'{exchange_id} --- {current_price} --- Цена меньше диапазона')
+
+    await asyncio.gather(*to_sell)
+
+
+async def limit_sell_order(exchange, symbol, price):
+    """Создаём ордер на продажу всего баланса по текущей цене минус какой то процент"""
+
+    balance = await exchange[1].fetch_balance({'type':'spot'})
+
+    amount = balance.get('SUI').get('free')
+
+    if amount > 0.1:
+
+
+        try:
+            logger.info(f'{exchange[0]} cоздаём ордер на продажу в {exchange[1].id}. Amount = {amount} Price = {price}')
+
+            await exchange[1].create_limit_sell_order(
+                symbol,
+                amount,
+                price,
+            )
+            balance = await exchange[1].fetch_balance({'type': 'spot'})
+            availableamount = balance.get('SUI').get('free')
+            usdtamount = balance.get('USDT').get('free')
+            logger.info(F'{exchange[0]} {exchange[1]} остаток SUI - {availableamount}, баланс USDT - {usdtamount}')
+        except Exception as e:
+            return logger.error(f'Не удалось создать ордер в {exchange[1].id}. {e}')
+
+
+async def main():
+    exchanges, exchanges_set = await services.api_settings()
+    if not exchanges:
+        return  logger.error('Добавьте данные о биржах в файл `api.json`')
+
+    range_start, range_end, insta_sell, max_time_in_range, percent = services.price_settings()
+    if not range_start or not range_end or not max_time_in_range:
+        return logger.error('Добавьте данные о ценах в файл `prices.json`')
+
+
+    while True:
+        await check_price(exchanges, exchanges_set, range_start, range_end,insta_sell, percent,max_time_in_range)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ services.py
@@ -0,0 +1,68 @@
+import asyncio
+import ccxt.async_support as ccxt
+import json
+from loguru import logger
+
+
+async def api_settings():
+    global check_tasks
+    count = 1
+
+    exchanges = dict()
+    exchanges_set = set()
+
+    with open('api.json') as file:
+        api_data = json.load(file)
+
+    while api_data.get(f'account{count}'):
+        account_data = api_data.get(f'account{count}', {})
+        
+        check_tasks = []
+
+        kucoin_data = account_data.get('kucoin', {})
+        if kucoin_data.get('apiKey') and kucoin_data.get('secret') and kucoin_data.get('password') and kucoin_data.get('proxies'):
+
+            exchange = ccxt.kucoin(kucoin_data)
+            check_tasks.append(checkAndTransfer(exchange))
+            exchanges.setdefault('kucoin', []).append((f'account{count}', exchange))
+            exchanges_set.add((ccxt.kucoin(), 0))
+
+        bybit_data = account_data.get('bybit', {})
+        if bybit_data.get('apiKey') and bybit_data.get('secret') and bybit_data.get('proxies'):
+
+            exchange = ccxt.bybit(bybit_data)
+            check_tasks.append(checkAndTransfer(exchange))
+            exchanges.setdefault('bybit', []).append((f'account{count}', exchange))
+            exchanges_set.add((ccxt.bybit(), 0))
+
+        count += 1
+
+    await asyncio.gather(*check_tasks)
+
+    return exchanges, list(exchanges_set)
+
+
+async def checkAndTransfer(exchange):
+    balance = await exchange.fetch_balance({'type': 'funding'})
+
+    if 'SUI' in balance:
+        amount = balance.get('SUI').get('free')
+
+        if amount > 0:
+            await exchange.transfer('SUI', amount, 'funding', 'spot')
+            logger.info(f"Отправил SUI на спот")
+        else:
+            logger.info(f'SUI уже на споте')
+
+
+def price_settings():
+    with open('prices.json') as file:
+        api_data = json.load(file)
+
+    range_start = api_data.get('range_start')
+    range_end = api_data.get('range_end')
+    insta_sell = api_data.get('insta_sell')
+    max_time_in_range = api_data.get('max_time_in_range')
+    percent = api_data.get('percent')
+
+    return range_start, range_end, insta_sell, max_time_in_range, percent
